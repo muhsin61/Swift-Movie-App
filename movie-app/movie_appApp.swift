898e115a126917ef572a8aa061ac843cb7c932c4
Muhsin Köse <36308247+muhsin61@users.noreply.github.com>
2022-06-12 12:25:56 +0300

    first commit

---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Muhsin Köse on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
